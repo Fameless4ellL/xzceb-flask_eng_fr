162498af4048beb5485b43355eebe41fe5101ef5
your_github_username <youremail@domain.com>
2022-12-20 09:43:11 -0500

    Final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,19 @@
+import unittest
+from translator import english_to_french, french_to_english
+
+class TestEnglishToFrench(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(english_to_french(), None)
+        self.assertEqual(
+            english_to_french('Hello')['translations'][0]['translation'],
+            'Bonjour'
+        )
+
+
+class TestFrenchToEnglish(unittest.TestCase):
+    def test2(self):
+        self.assertEqual(french_to_english(), None)
+        self.assertEqual(
+            french_to_english('Bonjour')['translations'][0]['translation'],
+            'Hello'
+        )
---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,41 @@
+"""
+made for ibm
+"""
+
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def english_to_french(french_text: str = None):
+    """
+    english_to_french
+    """
+    french_text = language_translator.translate(
+        text=french_text, model_id='en-fr'
+    ).get_result()
+    return french_text
+
+def french_to_english(english_text: str = None):
+    """
+    french_to_english
+    """
+    english_text = language_translator.translate(
+        text=english_text, model_id='fr-en'
+    ).get_result()
+    return english_text
+    
